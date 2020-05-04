502142dd1c237c429d281b94e1a4ea9dc76a64ee
Tuan <doanhtuan14111997@gmail.com>
2020-05-04 17:55:45 +0700

    t

--- app/settings.py
+++ app/settings.py
@@ -122,6 +122,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
+ALLOWED_HOSTS = ['appwebsamsung.herokuapp.com/']
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
